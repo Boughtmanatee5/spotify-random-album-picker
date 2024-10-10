267ca0e4431877582a88292425d9474337f34f44
Boughtmanatee5 <seancostellodp@gmail.com>
2024-10-09 21:15:17 -0400

    Add vite-plugin-require for spotify api using crypto

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import require from 'vite-plugin-require';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), require()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
