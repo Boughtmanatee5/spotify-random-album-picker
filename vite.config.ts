d10e55a7f2eb771ffa962cb20f4174594148152c
Boughtmanatee5 <seancostellodp@gmail.com>
2024-10-10 11:20:24 -0400

    Remove vite-plugin-require

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import vitePluginRequire from 'vite-plugin-require';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		// @ts-expect-error
-		vitePluginRequire.default()
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
